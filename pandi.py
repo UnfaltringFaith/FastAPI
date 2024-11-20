9954ff14229ba441c45e9e6520f2448a9550ce5a
UnfalteringFaith <deniska45652@gmail.com>
2024-11-20 17:59:07 +0300

    zxczx

--- pandi.py
+++ pandi.py
@@ -6,3 +6,4 @@ rating = pd.read_csv("./rating.csv")
 #new = rating.merge(anime, left_on="anime_id", right_on="anime_id", suffixes=('_left', '_right'))
 
 print(anime.groupby("episodes").count())
+print("asd")
